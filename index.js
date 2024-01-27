95d68b1c2883ba0e4398e98462b0fee3a33f7d99
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-01-27 19:53:00 +0530

    pupiteer

--- index.js
+++ index.js
@@ -1,62 +1,31 @@
 import express from 'express'
-import  videoshow from 'videoshow'
+import videoshow from 'videoshow'
 import path from 'path'
 import nodeHtmlToImage from 'node-html-to-image'
+import puppeteer from 'puppeteer'
 
 const app = express()
 const port = 3000
 
 
 app.use(express.static('public'));
-// app.use("/video", async (req, res) => {
-//     const filePath = path.join(process.cwd(), 'public', 'output', 'new.mp4');
-
-//     var secondsToShowEachImage = 1
-//     var finalVideoPath = './test2.mp4'
-
-//     // setup videoshow options
-//     var videoOptions = {
-//         fps: 24,
-//         transition: false,
-//         videoBitrate: 1024,
-//         videoCodec: 'libx264',
-//         size: '640x640',
-//         outputOptions: ['-pix_fmt yuv420p'],
-//         format: 'mp4'
-//     }
-
-//     // array of images to make the 'videoshow' from
-//     var images = [
-//         { path: './group.png', loop: 5 },
-//         { path: './group.png', loop: 5 },
-//     ]
-
-//     videoshow(images, videoOptions)
-//         .save(filePath)
-//         .on('start', function (command) {
-//             console.log('encoding ' + finalVideoPath + ' with command ' + command)
-//         })
-//         .on('error', function (err, stdout, stderr) {
-//             return Promise.reject(new Error(err))
-//         })
-//         .on('end', function (output) {
-//             res.sendFile(filePath)
-//         })
-
-// })
 
 
-app.use("/", (req, res) => {
-    nodeHtmlToImage({
-        output: './image.png',
-        html: '<html><body>Hello world!</body></html>'
-      })
-        .then(() => {
-            res.sendFile(path.join(process.cwd(), 'image.png'))
-        })
-    // res.json({
-    //     message: "hello"
-    // })
+app.use("/screenshot", async (req, res) => {
+    const filePath = path.join(process.cwd(), 'public', 'output', `screenshot.png`);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.webpagetest.org/", { timeout: 0 });
+    await page.screenshot({ path: filePath });
+    await browser.close();
+    res.sendFile(filePath)
+})
+
+app.use("/", async (req, res) => {
+    res.json({
+        message: "hello"
+    })
 })
 
 
