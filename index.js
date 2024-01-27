6bcc4114cbf6e379e630012422ca3abef9056a09
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-01-27 18:12:39 +0530

    videoshow

--- index.js
+++ index.js
@@ -7,42 +7,42 @@ const port = 3000
 
 
 app.use(express.static('public'));
-app.use("/video", async (req, res) => {
-    const filePath = path.join(process.cwd(), 'public', 'output', 'new.mp4');
+// app.use("/video", async (req, res) => {
+//     const filePath = path.join(process.cwd(), 'public', 'output', 'new.mp4');
 
-    var secondsToShowEachImage = 1
-    var finalVideoPath = './test2.mp4'
+//     var secondsToShowEachImage = 1
+//     var finalVideoPath = './test2.mp4'
 
-    // setup videoshow options
-    var videoOptions = {
-        fps: 24,
-        transition: false,
-        videoBitrate: 1024,
-        videoCodec: 'libx264',
-        size: '640x640',
-        outputOptions: ['-pix_fmt yuv420p'],
-        format: 'mp4'
-    }
+//     // setup videoshow options
+//     var videoOptions = {
+//         fps: 24,
+//         transition: false,
+//         videoBitrate: 1024,
+//         videoCodec: 'libx264',
+//         size: '640x640',
+//         outputOptions: ['-pix_fmt yuv420p'],
+//         format: 'mp4'
+//     }
 
-    // array of images to make the 'videoshow' from
-    var images = [
-        { path: './group.png', loop: 5 },
-        { path: './group.png', loop: 5 },
-    ]
+//     // array of images to make the 'videoshow' from
+//     var images = [
+//         { path: './group.png', loop: 5 },
+//         { path: './group.png', loop: 5 },
+//     ]
 
-    videoshow(images, videoOptions)
-        .save(filePath)
-        .on('start', function (command) {
-            console.log('encoding ' + finalVideoPath + ' with command ' + command)
-        })
-        .on('error', function (err, stdout, stderr) {
-            return Promise.reject(new Error(err))
-        })
-        .on('end', function (output) {
-            res.sendFile(filePath)
-        })
+//     videoshow(images, videoOptions)
+//         .save(filePath)
+//         .on('start', function (command) {
+//             console.log('encoding ' + finalVideoPath + ' with command ' + command)
+//         })
+//         .on('error', function (err, stdout, stderr) {
+//             return Promise.reject(new Error(err))
+//         })
+//         .on('end', function (output) {
+//             res.sendFile(filePath)
+//         })
 
-})
+// })
 
 
 app.use("/", (req, res) => {
