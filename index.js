109c944cb88641b7824c6c4e32a79510fef5125e
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-01-27 20:09:41 +0530

    chrome aws lambda

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import videoshow from 'videoshow'
 import path from 'path'
 import nodeHtmlToImage from 'node-html-to-image'
 import puppeteer from 'puppeteer'
+import chromium from 'chrome-aws-lambda';
 
 const app = express()
 const port = 3000
@@ -14,7 +15,19 @@ app.use(express.static('public'));
 app.use("/screenshot", async (req, res) => {
     const filePath = path.join(process.cwd(), 'public', 'output', `screenshot.png`);
 
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto("https://www.webpagetest.org/", { timeout: 0 });
+    // await page.screenshot({ path: filePath });
+    // await browser.close();
+    // res.sendFile(filePath)
+    const browser = await chromium.puppeteer.launch({
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    })
     const page = await browser.newPage();
     await page.goto("https://www.webpagetest.org/", { timeout: 0 });
     await page.screenshot({ path: filePath });
