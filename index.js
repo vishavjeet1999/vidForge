c69384495b7b7c661974c4a3dc202baeb9b82674
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-01-27 13:27:59 +0530

    videoshow

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 import express from 'express'
-import editily from 'editly'
+import  videoshow from 'videoshow'
 
 const app = express()
 const port = 3000
-app.use("/video", async(req, res)=>{
-    await editily({
-        width:900,
-        height: 1600,
-        outPath: "./test.mp4",
-        clips:[
-            {
-                duration: 10,
-                layers:[
-                    // {
-                    //     type: 'image',
-                    //     path: './group.png'
-                    // },
-                    {
-                        type:"news-title",
-                        text:"text 1"
-                    },
-                    {
-                        type:"subtitle",
-                        text: "text 2"
-                    }
-                ]
-            }
-        ]
-    }).catch(e=> console.log(e))
+app.use("/video", async (req, res) => {
+
+    var secondsToShowEachImage = 1
+    var finalVideoPath = './test2.mp4'
+
+    // setup videoshow options
+    var videoOptions = {
+        fps: 24,
+        transition: false,
+        videoBitrate: 1024,
+        videoCodec: 'libx264',
+        size: '640x640',
+        outputOptions: ['-pix_fmt yuv420p'],
+        format: 'mp4'
+    }
+
+    // array of images to make the 'videoshow' from
+    var images = [
+        { path: './group.png', loop: 5 },
+        { path: './group.png', loop: 5 },
+    ]
+
+    videoshow(images, videoOptions)
+        .save(finalVideoPath)
+        .on('start', function (command) {
+            console.log('encoding ' + finalVideoPath + ' with command ' + command)
+        })
+        .on('error', function (err, stdout, stderr) {
+            return Promise.reject(new Error(err))
+        })
+        .on('end', function (output) {
+            // do stuff here when done
+        })
 
     res.json(req.query)
 })
 
-app.use("/", (req, res)=>{
+app.use("/", (req, res) => {
     res.json({
         message: "hello"
     })
 })
 
 
-app.listen(port, ()=> { console.log(`Server running on port ${port}`) })
+app.listen(port, () => { console.log(`Server running on port ${port}`) })
